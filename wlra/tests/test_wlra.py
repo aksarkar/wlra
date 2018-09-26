bc5c9c0f9be1030a4020cf6cd32977da1ec453b6
Abhishek Sarkar <aksarkar@alum.mit.edu>
2018-09-26 11:04:22 -0500

    Fix tests

--- wlra/tests/test_wlra.py
+++ wlra/tests/test_wlra.py
@@ -13,16 +13,14 @@ def test_wlra_unit_weight():
   np.random.seed(0)
   x = np.random.normal(size=(100, 200))
   res = wlra.wlra(x, w=1, rank=1)
-  u0, d0, v0 = skd.PCA(n_components=1)._fit(x)
-  res0 = np.einsum('ij,j,jk->ik', u0, d0, v0)
+  res0 = wlra.lra(x, rank=1)
   assert np.isclose(res, res0).all()
 
 def test_wlra_rank_2():
   np.random.seed(0)
   x = np.random.normal(size=(100, 200))
   res = wlra.wlra(x, w=1, rank=2)
-  u0, d0, v0 = skd.PCA(n_components=2)._fit(x)
-  res0 = np.einsum('ij,j,jk->ik', u0, d0, v0)
+  res0 = wlra.lra(x, rank=2)
   assert np.isclose(res, res0).all()
 
 def test_pois_lra_shape():
@@ -37,3 +35,8 @@ def test_pois_lra_masked_array():
   res = wlra.pois_lra(x, 1)
   assert res.shape == (100, 200)
   assert not np.ma.is_masked(res)
+
+def test_hsvd():
+  x = np.zeros((100, 200))
+  res = wlra.hsvd(x, s=1, rank=1)
+  assert res.shape == (100, 200)
